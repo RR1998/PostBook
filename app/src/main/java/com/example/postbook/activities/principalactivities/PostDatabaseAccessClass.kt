1a61fe0682f445cc2ff2aeaa395b42bce68bd148
RR <Roberto_Rubio98@outlook.com>
2019-12-14 17:02:43 -0600

    Added PostDatabaseAccessClass :sparkles:

---
+++ app/src/main/java/com/example/postbook/activities/principalactivities/PostDatabaseAccessClass.kt
@@ -0,0 +1,37 @@
+package com.example.postbook.activities.principalactivities
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.postbook.postclassmodels.PostClass
+
+class PostDatabaseAccessClass(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+                              SQLiteOpenHelper(context,
+                                  DATABASE_NAME, factory,
+                                  DATABASE_VERSION
+                              ) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+    fun addName(post: PostClass) {
+        val values = ContentValues()
+        values.put("_id", post.id)
+        values.put("userid", post.userId)
+        values.put("title", post.title)
+        values.put("body", post.body)
+        val db = this.writableDatabase
+        db.insert("posts", null, values)
+        db.close()
+    }
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "post.db"
+    }
+}
