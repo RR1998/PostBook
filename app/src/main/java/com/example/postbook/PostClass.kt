3337e2bf5f081c0477119eb92b3a2239cd095175
RR <Roberto_Rubio98@outlook.com>
2019-12-14 13:34:58 -0600

    Refactored PostClass properties :recycle:

--- app/src/main/java/com/example/postbook/PostClass.kt
+++ app/src/main/java/com/example/postbook/PostClass.kt
@@ -2,7 +2,7 @@ package com.example.postbook
 
 class PostClass {
     var userId: Long = 0
-    var postId: Long = 0
-    var postTitle: String = ""
-    var postBody: String = ""
+    var id: Long = 0
+    var title: String = ""
+    var body: String = ""
 }
