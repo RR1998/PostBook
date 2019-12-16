0e7a220cab52e732f743ae63db635d1057061d6f
RR <Roberto_Rubio98@outlook.com>
2019-12-16 09:28:42 -0600

    Added CommentBundleObject :sparkles:

---
+++ app/src/main/java/com/example/postBook/activities/adapter/CommentBundleObject.kt
@@ -0,0 +1,9 @@
+package com.example.postBook.activities.adapter
+
+object CommentBundleObject {
+
+    const val userId = "userIdSelected"
+    const val tittleId = "commentSelected"
+    const val commentTittle = "tittleSelected"
+    const val commentBody = "bodySelected"
+}
