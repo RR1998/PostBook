e360d8bf0d0c8614f79bd562adcae4d40ecc528a
RR <Roberto_Rubio98@outlook.com>
2019-12-16 09:28:12 -0600

    Added PostBookConstantsObject :recycle:

---
+++ app/src/main/java/com/example/postBook/activities/principalActivities/PostBookConstantsObject.kt
@@ -0,0 +1,12 @@
+package com.example.postBook.activities.principalActivities
+
+object PostBookConstantsObject {
+
+    const val readTimeOut = 10000
+    const val connectTimeOut = 10000
+    const val methodGet = "GET"
+    const val protocol = "https"
+    const val UrlHost = "jsonplaceholder.typicode.com"
+    const val commentUrlFile = "comments?postId="
+    const val postUrlFile = "posts"
+}
