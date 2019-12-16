ff4702e842bbed62e67b2aad1405d68113333306
RR <Roberto_Rubio98@outlook.com>
2019-12-15 20:21:01 -0600

    Refactored code :recycle:

--- app/src/main/java/com/example/postBook/postClassModels/PostCommentClass.kt
+++ app/src/main/java/com/example/postBook/postClassModels/PostCommentClass.kt
@@ -1,9 +1,9 @@
 package com.example.postBook.postClassModels
 
-class PostComentClass {
+class PostCommentClass {
     var postId: Long = 0 //this will be use as foreign key
     var id: Long = 0 //This is the primary key
-    var userName: String = ""
+    var name: String = ""
     var email:String = ""
     var body: String = ""
 }
